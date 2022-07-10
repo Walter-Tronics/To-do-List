3fa1bb311985055122032ad7364e06f2a674f77f
Walter <waltertronics02@gmail.com>
2022-07-10 17:03:57 +0100

    Testing the save functionality

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ $(function(){
             //adding the task object to the task array
             taskArray.push(task);
 
+            /*
             //Create a new list item
             var elem = $("<li></li>").text(val);
 
@@ -37,6 +38,9 @@ $(function(){
 
             //Append the new list item to the list
             $("#mylist").append(elem);
+            */
+
+            getTasks();
 
             //Clear the input field
             $("input").val("");
@@ -76,4 +80,27 @@ $(function(){
         //Get the stored value of the task array
         console.log(taskArray);
     });
+
+    //Creating a funtion for retrieving the tasks from local storage
+    function getTasks(){
+        //Get the stored value of the task array
+        //taskArray = localStorage.getItem("Tasks");
+
+        //Loop through the task array
+        for (var i = 0; i < taskArray.length; i++) {
+            //Create a new list item
+            var elem = $("<li></li>").text(taskArray[i].task);
+
+            //Append a delete button to the list item
+            $(elem).append("<button class=' rem btn btn-sm btn-danger' style='float: right;'>&times;</button>");
+
+            //Append a click button to the list item
+            $(elem).append("<button class='mark btn btn-sm btn-success' style='float: right;'>&check;</button>");
+
+            //Append the new list item to the list
+            $("#mylist").append(elem);
+        }
+    }
+
+    getTasks();
 });
